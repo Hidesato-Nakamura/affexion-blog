0e5fcc8041e9393caab6f8ea737bec598a678559
Hidesato <>
2020-11-18 19:27:12 +0900

    css

--- src/components/ranking.js
+++ src/components/ranking.js
@@ -71,11 +71,7 @@ const Ranking = ({ data }) => {
       <div className="ranking-card flex">
         {postResults.map((postResult, index) => {
           return index < max ? (
-            <article
-              id={`ScrollCard${index}`}
-              key={index}
-              className="cards sub-card-block"
-            >
+            <article id={`ScrollCard${index}`} key={index} className="cards">
               <SubCard node={postResults[index]} rank={index + 1} />
             </article>
           ) : null
