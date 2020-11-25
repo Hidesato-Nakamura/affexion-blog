210079dd55b4911fe2e82783a04b6a003edfe1ae
Hidesato <>
2020-11-25 21:32:44 +0900

    caa

--- src/components/card.js
+++ src/components/card.js
@@ -4,7 +4,26 @@ import { Link } from "gatsby"
 export const MainCard = ({ node }) => {
   const frontmatter = node.frontmatter
   const slug = node.fields.slug
-  let card
+  let card = (
+    <Link to={slug} style={{ textDecoration: "none" }}>
+      <section className="main-card flex">
+        <div className="card-img-block main">
+          <img src={frontmatter.featuredimage} alt="" />
+        </div>
+
+        <div className="card-content">
+          <div className="tag flex">
+            {frontmatter.tags.map((tag, index) => {
+              return <div key={index}>#{tag}</div>
+            })}
+          </div>
+          <div className="title">{frontmatter.title}</div>
+          <div className="date">{frontmatter.date}</div>
+          <div className="text">{frontmatter.description}</div>
+        </div>
+      </section>
+    </Link>
+  )
   return card
 }
 
