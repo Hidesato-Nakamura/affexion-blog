98a92d3b88e700fab5096367bfb6ed519d7a552e
Hidesato <>
2020-12-24 18:35:51 +0900

    sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,6 +93,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-feed`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
