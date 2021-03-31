c8edc8b85c8e0c2bcf5dc0cfa4c6e2c630be6bc9
Hidesato <>
2021-03-31 17:16:02 +0900

    test

--- src/components/card.js
+++ src/components/card.js
@@ -41,7 +41,7 @@ export const Card = ({ node }) => {
   tags.forEach((tag, index) => {
     tags[index] = tag.toLowerCase()
   })
-  const imageName = frontmatter.split("/")[4]
+  const imageName = frontmatter.featuredimage.split("/")[4]
   let card = (
     <Link to={slug} style={{ textDecoration: "none" }}>
       <section className="sub-card">
